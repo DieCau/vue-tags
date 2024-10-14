73f5e7c62b9c289262b80cd04ba3adb80764ce7f
Diego Caucota <diegocaucota@gmail.com>
2024-10-14 08:52:46 -0300

    add: InputTag

---
+++ src/components/InputTag.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            currentValue: "",
+            tags: [],
+        }
+    },
+    methods: {
+        handleKeyDown(e) {
+            if (e.key === "Enter" && this.currentValue !== "") {
+                this.tags.push(this.currentValue)          
+            }
+        },
+    },
+}
+</script>
+
+<template>
+    <div class="inputTag">
+        <div class="tags">
+            <div class="tag" v-for="(tag, index) in tags" :key="index">
+                {{ tag }}
+            </div>
+        </div>
+        <input type="text" v-model="currentValue" @keyDown="handleKeyDown" />
+    </div>
+</template>
+<style scoped></style>
